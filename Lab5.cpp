1f5dcbf50918b41baf2f7a5655cce9b1f219e776
Bogatu Alexei <alexeibogatu@gmail.com>
2026-04-23 22:37:55 +0300

    Fixed errors

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,9 +1,9 @@
+#include <format>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
-
 class Cont{
     protected:
         int ID;
@@ -17,68 +17,57 @@ class Cont{
             sold = 0;
             tranzactie = {};
         }
-        
         Cont(int a, string b, float c, vector<string> d){
-            ID = a;
-            nume = b;
-            sold = c;
-            tranzactie = d;
+            ID = a; nume = b; sold = c; tranzactie = d;
         }
-
         ~Cont(){}
-
         void setId(int x){ID = x;}
         void setNume(string x){nume = x;}
         void setSold(float x){sold = x;}
         void setTranzactie(vector<string> x){tranzactie = x;}
-
         int getID(){return ID;}
         string getNume(){return nume;}
         float getSold(){return sold;}
         vector<string> getTranzactie(){return tranzactie;}
         string getTranzactieI(int i){return tranzactie[i];}
-
         void depuneBani(float suma){
             sold += suma;
             cout << "Depunerea a fost efectuata cu succes" << endl;
             cout << endl;
-
             string s;
-            s = "Depunere bani -> suma: " + to_string(suma);
+            char buf[100];
+            snprintf(buf, sizeof(buf), "Depunere bani -> suma: %.2f => sold: %.2f", suma, sold);
+            s = buf;
             tranzactie.push_back(s);
         }
-
         void retrageBani(float suma){
             if(suma <= sold){
                 sold -= suma;
                 cout << "Extragerea a fost efectuata cu succes" << endl;
                 cout << endl;
-
                 string s;
-                s = "Retragere bani -> suma: " + to_string(suma);
+                char buf[100];
+                snprintf(buf, sizeof(buf), "Retragere bani -> suma: %.2f => sold: %.2f", suma, sold);
+                s = buf;
                 tranzactie.push_back(s);
             } else {
-                cout << "Fonduri insuficiente" << endl;
-                cout << endl;
+                cout << "Fonduri insuficiente" << endl << endl;
             }
         }
-
         void trimiteBani(int idDestinatie, vector<Cont> &C){
             for(int i=0; i<C.size(); i++){
                 if(idDestinatie == C[i].getID()){
                     float suma;
-
                     cout << "Introduceti suma pentru transfer" << endl;
                     cout << "Suma: "; cin >> suma;
-                    
                     if(suma <= sold){
                         sold -= suma;
                         C[i].setSold(C[i].getSold() + suma);
-
                         cout << "Transferul a fost efectuata cu succes" << endl << endl;
-
                         string s;
-                        s = "Transfer -> persoana: " + C[i].getNume() + " -> suma -> " + to_string(suma);
+                        char buf[100];
+                        snprintf(buf, sizeof(buf), "Transfer -> persoana: %s -> suma -> %.2f", C[i].getNume().c_str(), suma);
+                        s = buf;
                         tranzactie.push_back(s);
                     } else {
                         cout << "Fonduri insuficiente" << endl << endl;
@@ -86,22 +75,18 @@ class Cont{
                     return;
                 }
             }
-            cout << "Error! Nu exista cont cu ID-ul oferit" << endl;
-            cout << endl;
+            cout << "ID invalid." << endl << endl;
         }
-
         void verificaSold(){
             cout << "Sold: " << sold << endl;
             cout << endl;
         }
-
         void vizualizeazaTranzactii(){
             for(int i = 0; i < tranzactie.size(); i++){
-                cout << i << ") " << tranzactie[i] << endl;
+                cout << i+1 << ") " << tranzactie[i] << endl;
             }
             cout << endl;
         }
-
         void blocheazaCont(int a, vector<Cont> &C){
             for(int i=0; i<C.size(); i++){
                 if(C[i].getID() == a){
@@ -113,7 +98,6 @@ class Cont{
             }
         }
 };
-
 void citire(ifstream &Fi, vector<Cont> &C){
     int a; string b; float c; int n;
     while(Fi >> a){
@@ -121,7 +105,6 @@ void citire(ifstream &Fi, vector<Cont> &C){
         Fi >> c;
         Fi >> n;
         Fi.ignore();
-        
         vector<string> d;
         for(int i = 0; i < n; i++){
             string line;
@@ -132,7 +115,6 @@ void citire(ifstream &Fi, vector<Cont> &C){
     }
     Fi.close();
 }
-
 void scriere(ofstream &Fo, vector<Cont> &C){
     for(int i = 0; i < C.size(); i++){
         Fo << C[i].getID() << '\n';
@@ -146,24 +128,20 @@ void scriere(ofstream &Fo, vector<Cont> &C){
     }
     Fo.close();
 }
-
 void interfata(int a, vector<Cont> &C){
     int x;
     do{
-        cout << "Detinator: " << C[a].getNume() << endl;
+        cout << "ATM (Detinator: " << C[a].getNume() << ")" << endl;
         cout << "1 => Depunere numerar" << endl;
         cout << "2 => Extragere numerar" << endl;
         cout << "3 => Transfer numerar catre alt cont" << endl;
         cout << "4 => Afisarea soldului" << endl;
         cout << "5 => Istoria tranzactiilor" << endl;
-        cout << "6 => Blocheaza contul" << endl;
-        cout << endl;
+        cout << "6 => Blocheaza contul" << endl << endl;
         cout << "0 => Iesire" << endl;
-
         cout << "Actiune: ";
         cin >> x;
         cout << endl;
-
         switch(x){
             case 0:{
                 cout << endl;
@@ -191,6 +169,10 @@ void interfata(int a, vector<Cont> &C){
                 int y;
                 cout << "Introduceti ID-ul contului destinat" << endl;
                 cout << "ID: "; cin >> y;
+                if(y == C[a].getID()){
+                    cout << "ID invalid" << endl;
+                    return;
+                }
                 C[a].trimiteBani(y, C);
                 ofstream Fo("conturi.txt");
                 scriere(Fo, C);
@@ -210,17 +192,21 @@ void interfata(int a, vector<Cont> &C){
                 scriere(Fo, C);
                 return;
             }
-            default: cout << "Nu exista optinunea selectata" << endl;
+            default: cout << "Operatie nevalida" << endl;
         }
-
     }while(true);
 }
-
 void inregistrare(vector<Cont> &C){
     Cont A;
     int a; string b;
     cout << "Creati un ID" << endl;
     cout << "ID: "; cin >> a;
+    for(int i=0; i<C.size(); i++){
+        if(a == C[i].getID()){
+            cout << "Cont cu ID-ul oferit deja exista" << endl << endl;
+            return;
+        }
+    }
     cout << "Introduceti numele dumneavoastra" << endl;
     cout << "Nume: "; cin >> b;
     A.setId(a);
@@ -229,41 +215,28 @@ void inregistrare(vector<Cont> &C){
     cout << endl;
     interfata(C.size()-1, C);
 }
-
 void autentificare(vector<Cont> &C){
-    int x;
-    bool a = false;
+    int id, index = -1;
     cout << "Introduceti ID-ul contului: ";
-    cin >> x;
-    for(int i=0; i<C.size(); i++){
-        if(C[i].getID() == x){
-            x = i;
-            a = true;
-            break;
-        }
-    }
-    if(a){
-        cout << endl;
-        interfata(x, C);
-    } else {
-        cout << "Error! Nu exista cont cu ID-ul oferit." << endl;
+    cin >> id;
+    for (int i = 0; i < C.size(); i++) 
+        if (C[i].getID() == id) { index = i; break; }
+    if (index != -1) interfata(index, C);
+    else {
+        cout << "Date gresite" << endl;
         cout << endl;
     }
 }
-
 void menu(vector<Cont> &C){
     int x;
     do{
         cout << "ATM" << endl;
         cout << "1) Inregistrare" << endl;
-        cout << "2) Autentificare" << endl;
-        cout << endl;
+        cout << "2) Autentificare" << endl << endl;
         cout << "0) Iesire" << endl;
-
         cout << "Actiune: ";
         cin >> x;
         cout << endl;
-        
         switch(x){
             case 0:{
                 return;
@@ -276,22 +249,16 @@ void menu(vector<Cont> &C){
                 autentificare(C);
                 break;
             }
-            default: cout << "Nu exista optinunea selectata" << endl;
+            default: cout << "Operatie nevalida" << endl;
         }
-    }while(true);
-    
+    }while(true); 
 }
-
 int main(){
     vector<Cont> C;
-
     ifstream Fi("conturi.txt");
     citire(Fi, C);
-    
     menu(C);
-
     ofstream Fo("conturi.txt");
     scriere(Fo, C);
-
     return 0;
 }
